b8f6031077a28c9d1a2bb1b31cab3c87c2187d2f
Mnex0 <matthieu.head@gmail.com>
2025-03-01 13:42:41 +0100

    ex2 working

--- ComWeb/TP/TPJSON/ex2/js/polls.js
+++ ComWeb/TP/TPJSON/ex2/js/polls.js
@@ -1,46 +1,5 @@
 'use strict';
 
-async function requestPolls()
-{
-  const response = await fetch('php/request.php/polls');
-  addEventListener(document.getElementById('polls-select').value, requestPoll(document.getElementById('polls-select').value));
-  if (!response.ok)
-  {
-    displayError(response.status);
-  }
-  else
-    displayPolls(await response.json());
-}
-
-function displayPolls(polls)
-{
-    for (let i = 0; i < 3; i++)
-    {
-      document.getElementById('polls-select').innerHTML += "<option value='"+i+"'>"+polls[i]["title"]+"</option>";
-    }
-}
-
-async function requestPoll(id)
-{
-  const response = await fetch('php/request.php/polls/'.concat(id+1));
-  if (!response.ok)
-  {
-    displayError(response.status);
-  }
-  else
-    displayPoll(await response.json());
-}
-
-function displayPoll(poll)
-{
-  for (let i = 1; i < 4; i++)
-  {
-    let score = (100*parseFloat(poll['option'+i+'score'])/parseFloat(poll.participants)).toFixed(2);
-    document.getElementById('poll-option'+i).innerHTML = '<div class="progress" role="progressbar" aria-label="'+poll["option"+i]+'" aria-valuenow="'+score+'" aria-valuemin="0" aria-valuemax="100"><div class="progress-bar" style="width: '+score+'"></div>';
-  }
-  
-}
-
 function displayError(error)
 {
     let messages =
@@ -56,4 +15,75 @@ function displayError(error)
     document.getElementById('errors').innerHTML = '<i class="fa-solid fa-circle-xmark"></i>' + " Error " + error.status + " : " + messages[error.status];
 }
 
-requestPolls();
+async function requestPolls()
+{
+  const response = await fetch('php/request.php/polls');
+  if (!response.ok)
+  {
+    displayError(response.status);
+  }
+  else
+    displayPolls(await response.json());
+}
+
+async function requestPoll(id)
+{
+  const response = await fetch('php/request.php/polls/'.concat(id+1));
+  if (!response.ok)
+  {
+    displayError(response.status);
+  }
+  else
+    displayPoll(await response.json());
+}
+
+function displayPolls(polls)
+{
+    for (let i = 0; i < 3; i++)
+    {
+      document.getElementById('polls-select').innerHTML += "<option value='"+(i+1)+"'>"+polls[i]["title"]+"</option>";
+    }
+}
+
+function displayPoll(poll)
+{
+  for (let i = 1; i < 4; i++)
+  {
+    let score = (100*parseFloat(poll['option'+i+'score'])/parseFloat(poll.participants)).toFixed(2);
+    document.getElementById('poll-title').innerHTML = `
+      <div class="d-flex justify-content-between">
+        <span>${poll["title"]}</span>
+        <span class="badge rounded-pill text-bg-primary">${poll.participants}</span>
+      </div>`;
+    document.getElementById('poll-option'+i).innerHTML = `
+      ${poll['option'+i]}
+      <div class="progress" role="progressbar" aria-label="${poll["option"+i]}" aria-valuenow="${score}" aria-valuemin="0" aria-valuemax="100">
+        <div class="progress-bar" style="width: ${score}%">${score} %</div>
+      </div>`;
+  }
+}
+
+function main()
+{
+  let selectElement = document.getElementById('polls-select');
+  selectElement.addEventListener("change", function()
+  {
+    if (selectElement.selectedIndex >= 0) {
+      requestPoll(selectElement.selectedIndex);
+    }
+  });
+  selectElement.addEventListener("input", function()
+  {
+    if (selectElement.selectedIndex >= 0) {
+      requestPoll(selectElement.selectedIndex);
+    }
+  });
+  requestPolls().then(() => 
+    {
+    if (selectElement.selectedIndex >= 0) {
+      requestPoll(selectElement.selectedIndex);
+    }
+  });
+}
+
+main();
